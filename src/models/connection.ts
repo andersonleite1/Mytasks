a7d1871b9a63a0bdf6b0a07eb5d97b480c5dae7f
Anderson Leite <andersonleite.dev@gmail.com>
2022-07-04 15:42:14 -0300

    feat(model): create connection  with db

---
+++ src/models/connection.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+import * as mysql from 'mysql2/promise';
+
+dotenv.config();
+
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+});
+
+export default connection;
