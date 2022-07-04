cf4576c16e42e4b6355339a4bb6e2002f68abac5
Anderson Leite <andersonleite.dev@gmail.com>
2022-07-04 15:38:48 -0300

    feat: config initial of API

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import * as express from "express";
+import * as cors from 'cors';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control_allow-origin', '*');
+      res.header('Access-Control_allow-Methods', 'GET,POST,DELETE,OPTIONS,PATCH,PUT');
+      res.header('Access-Control_allow-Headers', '*');
+      next();
+    }
+
+    this.app
+      .use(cors())
+      .use(express.json())
+      .use(accessControl);
+  }
+
+  public start(PORT: number | string): void {
+    this.app.get('/ping', (req, res, next) => {
+      return res.status(200).send('pingou');
+    });
+
+    this.app.listen(PORT, () => console.log('listening on port ', PORT));
+  }
+
+}
+
+export { App };
+export const { app } = new App();
